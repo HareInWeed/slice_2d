8d97b5e3ecc9559963eb303007613be5ccc3ed38
HareInWeed <zjnvv@163.com>
2021-04-12 09:32:14 +0800

    fix Eq impl

--- TODO.md
+++ TODO.md
@@ -6,4 +6,6 @@
 - [ ] more split methods
   - [ ] `split_parts`
   - [ ] `split_at_multiple`
+- [ ] more swap methods
+  - [ ] `swap_chunks`
 - [ ] 2d wrapper for `[T; N]` and `Vec<T>`
--- src/cmp.rs
+++ src/cmp.rs
@@ -3,22 +3,30 @@ use crate::{
     slice::{Shape2D, Slice2D, Slice2DMut, SlicePtr},
 };
 
+pub fn slice_2d_eq<T, A, B>(a: &A, b: &B) -> bool
+where
+    T: PartialEq,
+    A: Shape2D + SlicePtr<T> + Slice2DIter<T>,
+    B: Shape2D + SlicePtr<T> + Slice2DIter<T>,
+{
+    a.get_row() == b.get_row()
+        && a.get_col() == b.get_col()
+        && (a.get_slice_ptr() == b.get_slice_ptr()
+            || a.row_slice_iter()
+                .zip(b.row_slice_iter())
+                .all(|(a, b)| a == b))
+}
+
 impl<'a, T, A> PartialEq<A> for Slice2D<'a, T>
 where
     T: PartialEq,
     A: Shape2D + SlicePtr<T> + Slice2DIter<T>,
 {
     fn eq(&self, other: &A) -> bool {
-        self.get_row() == other.get_row()
-            && self.get_col() == other.get_col()
-            && (self.get_slice_ptr() == other.get_slice_ptr()
-                || self
-                    .row_slice_iter()
-                    .zip(other.row_slice_iter())
-                    .all(|(a, b)| a == b))
+        slice_2d_eq(self, other)
     }
 }
-impl<'a, T: PartialEq> Eq for Slice2D<'a, T> {}
+impl<'a, T: Eq> Eq for Slice2D<'a, T> {}
 
 impl<'a, T, A> PartialEq<A> for Slice2DMut<'a, T>
 where
@@ -26,13 +34,7 @@ where
     A: Shape2D + SlicePtr<T> + Slice2DIter<T>,
 {
     fn eq(&self, other: &A) -> bool {
-        self.get_row() == other.get_row()
-            && self.get_col() == other.get_col()
-            && (self.get_slice_ptr() == other.get_slice_ptr()
-                || self
-                    .row_slice_iter()
-                    .zip(other.row_slice_iter())
-                    .all(|(a, b)| a == b))
+        slice_2d_eq(self, other)
     }
 }
-impl<'a, T: PartialEq> Eq for Slice2DMut<'a, T> {}
+impl<'a, T: Eq> Eq for Slice2DMut<'a, T> {}
